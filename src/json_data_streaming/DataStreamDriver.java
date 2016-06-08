361649ef8e7b1ca3f771f9b5d7b968b917b23bb7
Jasper Lee <bobmugura@gmail.com>
2016-06-08 15:15:50 -0400

    finished JSON readin as HashMap object, creating a tally WIP

--- src/json_data_streaming/DataStreamDriver.java
+++ src/json_data_streaming/DataStreamDriver.java
@@ -1,5 +1,6 @@
 package json_data_streaming;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
@@ -13,74 +14,77 @@ import java.io.FileReader;
 import java.io.IOException;  
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;  
+import java.util.Map;
 
 public class DataStreamDriver {
 
 	public static void main(String[] args) {
 		Gson gson = new Gson();
+		DataStreamDriver driver = new DataStreamDriver();
 		
 		try {
 			System.out.println("Reading JSON from a file");
 			System.out.println("------------------------");
 			
 			BufferedReader br = new BufferedReader(new FileReader("/Users/Jasper/Documents/workspace/json_data_streaming/logs/logs_0.json"));
+
+			ArrayList logArrayList = driver.readJson(br);
+			System.out.println(logArrayList.get(0));
 			
-			//create logs object
-			Logs logsObj = gson.fromJson(br, Logs.class);
 			
-			System.out.println("Logs_id: " + logsObj.getId());
-			System.out.println("Logs count: " + logsObj.getLogs().length);
-			
-			Log[] logs = logsObj.getLogs();
-			System.out.println(logs[0]);
-			
-			for(int i=0; i<logsObj.getLogs().length; i++) {
-				
-			}
-		
-//			//read json file to String
-//			byte[] jsonData = Files.readAllBytes(Paths.get("/Users/Jasper/Documents/workspace/json_data_streaming/logs/logs_0.json"));
+//			//create logs object
+//			Logs logsObj = gson.fromJson(br, Logs.class);
 //			
-//			ObjectMapper objectMapper = new ObjectMapper();
+//			System.out.println("Logs_id: " + logsObj.getId());
+//			System.out.println("Logs count: " + logsObj.getLogs().length);
 //			
-//			JsonNode rootNode = objectMapper.readTree(jsonData);
-//			JsonNode idNode = rootNode.path("id");
-//			System.out.println("id = " + idNode.toString());
+//			Log[] logs = logsObj.getLogs();
+//			System.out.println(logs[0]);
 //			
-//			JsonNode emailNode = rootNode.path("email");
-//			JsonNode msgNode = rootNode.path("message");
-//			
-//			Iterator<JsonNode> elements = idNode.elements();
-//			while(elements.hasNext()) {
-//				System.out.println("HERE");
-//				JsonNode id = elements.next();
-//				System.out.println("ID = " + id.toString());
-//			}
-			
-//			JsonFactory jasonFactory = new JsonFactory();
-//			JsonParser jsonParser = jasonFactory.createParser("/Users/Jasper/Documents/workspace/json_data_streaming/logs/logs_0.json");
-//			System.out.println(jsonParser);
-//			while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
-//			   //get the current token
-//			   String fieldname = jsonParser.getCurrentName();
+//			for(int i=0; i<logsObj.getLogs().length; i++) {
 //				
-//			   if ("logs".equals(fieldname)) {
-//			      //move to next token
-//			      jsonParser.nextToken();
-//			      System.out.println(jsonParser.getText());        	 
-//			   }
 //			}
+		
+//			//Readin JSON and produce log message as HashMap object with ArrayList of logs
+//			Map<String,Object> map = new HashMap<String,Object>();
+//			map = (Map<String,Object>) gson.fromJson(br, map.getClass());
+//			System.out.println(map);
+//			System.out.println(map.get("logs"));
+//			System.out.println(map.get("logs").getClass());
+//			
+//			ArrayList logArrayList = (ArrayList) map.get("logs");
+//			
+//			System.out.println(logArrayList.get(0));
 			
-//			ObjectMapper mapper = new ObjectMapper();
-//			Logs logs = mapper.readValue("/Users/Jasper/Documents/workspace/json_data_streaming/logs/logs_0.json", Logs.class);
-//			System.out.println("here");  
 			
 			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+			
+		
+	} //end of main
+	
+	//Readin JSON and produce(return) log message as HashMap object with ArrayList of logs
+	public ArrayList<Log> readJson(BufferedReader br) {
+		Gson gson = new Gson();
+		Map<String,Object> map = new HashMap<String,Object>();
+		map = (Map<String,Object>) gson.fromJson(br, map.getClass());
+		System.out.println(map);
+		System.out.println(map.get("logs"));
+		System.out.println(map.get("logs").getClass());
+		
+		return (ArrayList) map.get("logs");
+	}
+	
+	//Consume log message and transform into a tally of all logs for each unique email address
+	//Also, add onto global tally
+	public void makeTally() {
+		
 	}
 }
